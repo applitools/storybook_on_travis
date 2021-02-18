3891b745b42d5ba64fd1c2be7e293d01a7cf6a38
Yarden Ingber <54064203+Yarden-Ingber@users.noreply.github.com>
2021-02-18 17:20:17 +0200

    Update applitools.config.1.js

--- applitools.config.1.js
+++ applitools.config.1.js
@@ -1,8 +1,8 @@
 module.exports = {
   concurrency: 40,
   showLogs: true,
-  appName: "Storybook_Test1",
-  batchName: "Storybook1",
+  appName: "LoadTest1",
+  batchName: "LoadTest1",
   storybookUrl: "https://jira-storybook.staging.atl-paas.net/?path=/story/*",
   readStoriesTimeout: 300000,
   waitBeforeScreenshots: 200,
