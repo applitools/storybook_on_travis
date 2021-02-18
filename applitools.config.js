434d38451e7a8e0278a749dd3e25a589c295c7f0
Yarden Ingber <54064203+Yarden-Ingber@users.noreply.github.com>
2021-02-18 16:08:13 +0200

    Update applitools.config.js

--- applitools.config.js
+++ applitools.config.js
@@ -7,7 +7,9 @@ module.exports = {
   readStoriesTimeout: 300000,
   waitBeforeScreenshots: 200,
   browser: [
-    {width: 800, height: 600, name: 'chrome'}
+    {width: 800, height: 600, name: 'chrome'},
+    {width: 800, height: 600, name: 'firefox'},
+    {width: 800, height: 600, name: 'safari'}
   ],
   serverUrl: 'https://test2eyes.applitools.com',
 };
