363fac0d1746e26854c50d8b67d6936508f81584
Yarden Ingber <54064203+Yarden-Ingber@users.noreply.github.com>
2021-02-18 16:59:56 +0200

    Create applitools.config.2.js

---
+++ applitools.config.2.js
@@ -0,0 +1,15 @@
+module.exports = {
+  concurrency: 40,
+  showLogs: true,
+  appName: "Storybook_Test",
+  batchName: "Storybook2",
+  storybookUrl: "https://jira-storybook.staging.atl-paas.net/?path=/story/*",
+  readStoriesTimeout: 300000,
+  waitBeforeScreenshots: 200,
+  browser: [
+    {width: 800, height: 600, name: 'chrome'},
+    {width: 800, height: 600, name: 'firefox'},
+    {width: 800, height: 600, name: 'safari'}
+  ],
+  serverUrl: 'https://test2eyes.applitools.com',
+};
