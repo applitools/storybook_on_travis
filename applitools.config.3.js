b01464d33e1ba634b05c07b12978f7adfd0c826b
Yarden Ingber <54064203+Yarden-Ingber@users.noreply.github.com>
2021-04-05 01:45:28 +0300

    Update applitools.config.3.js

--- applitools.config.3.js
+++ applitools.config.3.js
@@ -1,15 +1,13 @@
 module.exports = {
-  concurrency: 40,
-  showLogs: false,
+  concurrency: 100,
+  showLogs: true,
   appName: "LoadTest3",
   batchName: "LoadTest3",
-  storybookUrl: "https://jira-storybook.staging.atl-paas.net/?path=/story/*",
+  storybookUrl: "http://airbnb.io/react-dates/?path=/story/daterangepicker-drp--default",
   readStoriesTimeout: 300000,
   waitBeforeScreenshots: 200,
   browser: [
-    {width: 800, height: 600, name: 'chrome'},
-    {width: 800, height: 600, name: 'firefox'},
-    {width: 800, height: 600, name: 'safari'}
+    {width: 800, height: 600, name: 'chrome'}
   ],
   serverUrl: 'https://testeyes.applitools.com',
 };
