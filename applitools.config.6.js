9d967cbce278542683985fbd29d1d2a8e3eb82a6
Yarden.Ingber <yarden.ingber@applitools.com>
2021-04-14 14:28:14 +0300

    change concurrency to 50

--- applitools.config.6.js
+++ applitools.config.6.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testConcurrency: 20,
+  testConcurrency: 50,
   showLogs: true,
   appName: "LoadTest6",
   batchName: "LoadTest6",
--- applitools.config.9.js
+++ applitools.config.9.js
@@ -1,5 +1,5 @@
 module.exports = {
-  testConcurrency: 20,
+  testConcurrency: 50,
   showLogs: true,
   appName: "LoadTest9",
   batchName: "LoadTest9",
