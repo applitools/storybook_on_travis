45d06ac7f208ae459385cbb7dee9630ff198cbf9
Yarden.Ingber <yarden.ingber@applitools.com>
2021-04-16 16:35:48 +0300

    enable 32 machines

---
+++ applitools.config.21.js
@@ -0,0 +1,13 @@
+module.exports = {
+  testConcurrency: 50,
+  showLogs: true,
+  appName: "LoadTest21",
+  batchName: "LoadTest21",
+  storybookUrl: "http://airbnb.io/react-dates/?path=/story/daterangepicker-drp--default",
+  readStoriesTimeout: 300000,
+  waitBeforeScreenshots: 200,
+  browser: [
+    {width: 800, height: 600, name: 'chrome'}
+  ],
+  serverUrl: 'https://test2eyes.applitools.com',
+};
